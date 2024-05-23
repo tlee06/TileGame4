c18a66a36ea5f806e0324c3cfdcb938f4e0d2b0b
bbakshi16 <166397401+bbakshi16@users.noreply.github.com>
2024-05-23 19:00:33 -0400

    Added Comments

--- src/Input.java
+++ src/Input.java
@@ -9,19 +9,19 @@ public class Input {
     public static Keybind jump = new KeyboardKeybind(KeyEvent.VK_SPACE);
     public static Keybind zoomIn = new KeyboardKeybind(KeyEvent.VK_EQUALS);
     public static Keybind zoomOut = new KeyboardKeybind(KeyEvent.VK_MINUS);
-    public static Keybind chooseDirtBlockType = new KeyboardKeybind(KeyEvent.VK_1);
-    public static Keybind chooseStoneBlockType = new KeyboardKeybind(KeyEvent.VK_2);
-    public static Keybind noClip = new KeyboardKeybind(KeyEvent.VK_N);
-    public static Keybind moveUp = new KeyboardKeybind(KeyEvent.VK_W);
-    public static Keybind moveDown = new KeyboardKeybind(KeyEvent.VK_S);
-    public static Keybind reset = new KeyboardKeybind(KeyEvent.VK_R);
-    public static Keybind teleport = new KeyboardKeybind(KeyEvent.VK_T);
+    public static Keybind chooseDirtBlockType = new KeyboardKeybind(KeyEvent.VK_1); //added 
+    public static Keybind chooseStoneBlockType = new KeyboardKeybind(KeyEvent.VK_2);//added
+    public static Keybind noClip = new KeyboardKeybind(KeyEvent.VK_N); //added for noclip
+    public static Keybind moveUp = new KeyboardKeybind(KeyEvent.VK_W); //added for noclip
+    public static Keybind moveDown = new KeyboardKeybind(KeyEvent.VK_S);//added for noclip
+    public static Keybind reset = new KeyboardKeybind(KeyEvent.VK_R);//added for reset and teleport
+    public static Keybind teleport = new KeyboardKeybind(KeyEvent.VK_T);//added for reset and teleport
 
 
 
 
-    public static Keybind placeBlock = new MouseKeybind(MouseEvent.BUTTON3);
-    public static Keybind use = new MouseKeybind(MouseEvent.BUTTON1);
+    public static Keybind placeBlock = new MouseKeybind(MouseEvent.BUTTON3);//added for placing blocks
+    public static Keybind use = new MouseKeybind(MouseEvent.BUTTON1);//added for placing blocks
 
 
 
